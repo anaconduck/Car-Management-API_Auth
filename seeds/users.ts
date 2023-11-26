3c0c9a03c51c216dbe28c1f08bbdcaa3d39bd3be
Wahyu Kusuma Wardhana <wahyukusumaw29@gmail.com>
2023-11-26 15:05:34 +0700

    Update users.ts

--- seeds/users.ts
+++ seeds/users.ts
@@ -3,17 +3,12 @@ import bcrypt from "bcrypt";
 import dotenv from 'dotenv';
 dotenv.config();
 
+const hashedPassword = bcrypt.hashSync("12345678", 10);
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("users").del();
   
-    // Hash password using bcrypt
-    const passwordSeed = process.env.PASSWORD_SEED;
-  
-    // Check if rawPassword is defined before hashing
-    if (passwordSeed) {
-      const hashedPassword = await bcrypt.hash(passwordSeed, 10);
   
       // Inserts seed entries
       await knex("users").insert([
